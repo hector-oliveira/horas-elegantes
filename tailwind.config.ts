0cf629341c003e71044180aee33c9bb4e2cca4b1
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2024-03-12 17:33:43 -0300

    style: customizing the application theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,21 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+      colors: {
+        'brow-1': '#F8E1CF',
+        'brow-2': '#D8C2A7',
+        'brow-3': '#D2A27A',
+        'brow-4': '#763F21'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
