f91bf2d4975af11cffaafee12ba33e5cc88171a0
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2024-03-14 17:36:36 -0300

    chore: Input component export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Sidebar } from './Sidebar/Sidebar';
 export { BaseLayout } from './BaseLayout/BaseLayout';
 export { Header } from './Header/Header';
+export { Input } from './Input/@index';
