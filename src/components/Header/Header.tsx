92effea7cbe0151b453d87f37f250ff1cfb157ff
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2024-03-14 17:35:32 -0300

    feat: header component added and your functions

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,46 @@
-type HeaderProps = {
+'use client';
+import React, { useState } from 'react';
+import { Input } from '../Input/@index';
+
+interface HeaderProps {
   namePage: string;
-};
+}
+
 export const Header = ({ namePage }: HeaderProps) => {
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+
+  const handleStartDateChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setStartDate(event.target.value);
+  };
+
+  const handleEndDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEndDate(event.target.value);
+  };
+
+  const handleClick = () => {
+    alert(`Start Date: ${startDate}, End Date: ${endDate}`);
+  };
+
   return (
     <div className="w-full h-max flex justify-between px-10 py-20">
       <h1 className="text-2xl font-bold">{namePage}</h1>
-      <section className="flex justify-between items-center w-fit gap-6">
-        <input type="date" className="bg-white p-2 rounded-lg" />
+      <form className="flex justify-between items-center w-fit gap-6">
+        <Input.DateCalendar
+          value={startDate}
+          handleOnChange={handleStartDateChange}
+        />
         <p className="text-gray-400 font-normal text-sm">até</p>
-        <input type="date" className="bg-white p-2 rounded-lg" />
-      </section>
+        <Input.DateCalendar
+          value={endDate}
+          handleOnChange={handleEndDateChange}
+        />
+        <button className="bg-brow-3 p-2 rounded-lg" onClick={handleClick}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
