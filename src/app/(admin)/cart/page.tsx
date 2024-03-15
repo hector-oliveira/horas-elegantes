a019a60a3abe3c0e2e8f33016a26c010855870d5
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2024-03-15 16:31:25 -0300

    feat: adding a table on the cart page

--- src/app/(admin)/cart/page.tsx
+++ src/app/(admin)/cart/page.tsx
@@ -1,9 +1,37 @@
+import { TitleHeader } from '@/components/Table/TitleHeader';
 import { BaseLayout, Header } from '@/components/index';
+import { rowDatas } from './data/rowData';
+import { TableRow } from '@/components/Table/TableRow';
 
-export default function cart() {
+const headers = [
+  'Pedidos',
+  'Valor',
+  'Cliente',
+  'Data de Compra',
+  'Endereço de Entrega',
+  'Status'
+];
+
+export default function Cart() {
   return (
     <BaseLayout>
-      <Header namePage="Troca e Devoluções" />
+      <div className="w-full px-4">
+        <Header namePage="Troca e Devoluções" />
+        <main className="border border-black overflow-scroll max-h-96">
+          <table className="table-auto w-full border-spacing-y-6 border-separate">
+            <thead className="w-full">
+              <tr>
+                <TitleHeader titles={headers} />
+              </tr>
+            </thead>
+            <tbody className="w-full">
+              {rowDatas.map((row) => (
+                <TableRow key={row.id} {...row} />
+              ))}
+            </tbody>
+          </table>
+        </main>
+      </div>
     </BaseLayout>
   );
 }
