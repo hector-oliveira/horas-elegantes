210b5c340b3667db50796279a897a6950e4ca49f
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2024-03-15 16:32:48 -0300

    data: row data

---
+++ src/app/(admin)/cart/data/rowData.ts
@@ -0,0 +1,71 @@
+export const rowDatas = [
+  {
+    id: 1,
+    wishlist: 'FE3AB',
+    value: '209,10',
+    client: 'Rodrigo Gonçalves',
+    dateOfBuy: '10/03/2024',
+    address: 'Rua das Bobos, N° 0, SP',
+    status: 'Em Preparação'
+  },
+
+  {
+    id: 2,
+    wishlist: 'BR5TR',
+    value: '100,80',
+    client: 'Bumblebee',
+    dateOfBuy: '15/04/2022',
+    address: 'Rua Cybertron, N° 15, SC',
+    status: 'Enviado'
+  },
+
+  {
+    id: 3,
+    wishlist: 'EB1GV',
+    value: '198,30',
+    client: 'Mario Bross',
+    dateOfBuy: '05/07/2023',
+    address: 'Rua Nintendo, N° 64, SP',
+    status: 'Em Preparação'
+  },
+
+  {
+    id: 4,
+    wishlist: 'WN8UV',
+    value: '219,15',
+    client: 'Joaquim Alves',
+    dateOfBuy: '23/09/2020',
+    address: 'Rua das Pedras, N° 50, SP',
+    status: 'Em Preparação'
+  },
+
+  {
+    id: 5,
+    wishlist: 'CP4LA',
+    value: '204,95',
+    client: 'Waluigi',
+    dateOfBuy: '16/08/2020',
+    address: 'Rua nem queira saber, N° 64, RJ',
+    status: 'Enviado'
+  },
+
+  {
+    id: 6,
+    wishlist: 'NQ4XC',
+    value: '214,50',
+    client: 'Yosh',
+    dateOfBuy: '17/08/2020',
+    address: 'Ilha dos Dinossauros, N° 39, AC',
+    status: 'Enviado'
+  },
+
+  {
+    id: 7,
+    wishlist: 'HG6LR',
+    value: '164,15',
+    client: 'Ana Josefa',
+    dateOfBuy: '16/01/2019',
+    address: 'Rua das luzes, N° 24, MT',
+    status: 'Enviado'
+  }
+];
