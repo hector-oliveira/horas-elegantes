9755984ee923c73f70d33e6bda956cc97082882b
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2024-03-18 12:36:33 -0300

    improve: exporting the compositions of the table component

---
+++ src/components/Table/@index.tsx
@@ -0,0 +1,13 @@
+import { Row } from './Row';
+import { TitleHeader } from './TitleHeader';
+import { Head } from './Head';
+import { Body } from './Body';
+import { Root } from './Root';
+
+export const Table = {
+  Row,
+  TitleHeader,
+  Head,
+  Body,
+  Root
+};
