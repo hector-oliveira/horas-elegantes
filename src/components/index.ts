62a3ccdff22f9caf58a353fc21d01469995aa09c
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2024-03-18 12:32:19 -0300

    improve: export new component - Table

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { Sidebar } from './Sidebar/Sidebar';
 export { BaseLayout } from './BaseLayout/BaseLayout';
 export { Header } from './Header/Header';
 export { Input } from './Input/@index';
+export { Table } from './Table/@index';
