6e6f67744b18b778b94e9a39f57975990a89e142
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2024-03-19 15:31:18 -0300

    feat: implementing button actions functionalities

--- src/components/Table/Row.tsx
+++ src/components/Table/Row.tsx
@@ -1,3 +1,6 @@
+'use client';
+import { MdOutlineRemoveRedEye } from 'react-icons/md';
+import { TbEditCircle } from 'react-icons/tb';
 type RowDatasProps = {
   orderCode: string;
   value: string;
@@ -6,17 +9,38 @@ type RowDatasProps = {
   address: string;
   status: string;
   zebraEffect: string;
+  openModal: (code: string) => void;
 };
 
-const cellClassName = 'text-center py-4 px-6';
+const cellClassName = 'text-center py-4';
 
-export const Row = (props: RowDatasProps) => (
-  <tr className={props.zebraEffect}>
-    <td className={cellClassName}>{props.orderCode}</td>
-    <td className={cellClassName}>{props.value}</td>
-    <td className={cellClassName}>{props.client}</td>
-    <td className={cellClassName}>{props.dateOfBuy}</td>
-    <td className={cellClassName}>{props.address}</td>
-    <td className={cellClassName}>{props.status}</td>
-  </tr>
-);
+export const Row = (props: RowDatasProps) => {
+  return (
+    <tr className={props.zebraEffect}>
+      <td className={cellClassName}>{props.orderCode}</td>
+      <td className={cellClassName}>{props.value}</td>
+      <td className={cellClassName}>{props.client}</td>
+      <td className={cellClassName}>{props.dateOfBuy}</td>
+      <td className={cellClassName}>{props.address}</td>
+      <td className={`${cellClassName} flex justify-between item center gap-2`}>
+        {props.status}
+        <section className="flex flex-1 justify-end gap-4 pr-4">
+          <button onClick={() => props.openModal(props.orderCode)}>
+            <MdOutlineRemoveRedEye
+              size={22}
+              color="#5784E6"
+              className="cursor-pointer"
+            />
+          </button>
+          <button onClick={() => props.openModal(props.orderCode)}>
+            <TbEditCircle
+              size={22}
+              color="#F5C400"
+              className="cursor-pointer"
+            />
+          </button>
+        </section>
+      </td>
+    </tr>
+  );
+};
