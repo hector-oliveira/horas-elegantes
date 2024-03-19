25e1a1e78a31160815a5c52e90bce20ed91e9b7c
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2024-03-19 15:28:09 -0300

    feat: implementing Modal component functionalities

--- src/app/(admin)/cart/page.tsx
+++ src/app/(admin)/cart/page.tsx
@@ -1,6 +1,14 @@
 'use client';
-import { BaseLayout, Header, Input, Table } from '@/components/index';
+import {
+  BaseLayout,
+  Header,
+  Input,
+  Modal,
+  OrderDetails,
+  Table
+} from '@/components/index';
 import { useCart } from './utils/hooks/useCart';
+import { useCallback, useState } from 'react';
 
 const headers = [
   'Pedidos',
@@ -13,6 +21,31 @@ const headers = [
 
 export default function Cart() {
   const { filter, setFilter, filteredRowData, getZebraEffect } = useCart();
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [modalData, setModalData] = useState({
+    orderCodeValue: '',
+    statusValue: '',
+    clientValue: '',
+    addressValue: ''
+  });
+
+  const openModalWithOrderCode = useCallback(
+    (
+      orderCodeValue: string,
+      statusValue: string,
+      clientValue: string,
+      addressValue: string
+    ) => {
+      setModalData({ orderCodeValue, statusValue, clientValue, addressValue });
+      setIsOpenModal(true);
+    },
+    []
+  );
+
+  const closeModal = useCallback(() => {
+    setIsOpenModal(false);
+  }, []);
+
   return (
     <BaseLayout>
       <div className="w-full px-4 gap-2">
@@ -31,11 +64,27 @@ export default function Cart() {
                 <Table.Row
                   key={row.id}
                   zebraEffect={getZebraEffect(index)}
+                  openModal={() =>
+                    openModalWithOrderCode(
+                      row.orderCode,
+                      row.status,
+                      row.client,
+                      row.address
+                    )
+                  }
                   {...row}
                 />
               ))}
             </Table.Body>
           </Table.Root>
+          <Modal isOpen={isOpenModal} closeModal={closeModal}>
+            <OrderDetails
+              orderCodeValue={modalData.orderCodeValue}
+              clientValue={modalData.clientValue}
+              addressValue={modalData.addressValue}
+              statusValue={modalData.statusValue}
+            />
+          </Modal>
         </main>
       </div>
     </BaseLayout>
