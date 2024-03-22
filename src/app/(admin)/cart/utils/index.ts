8bdc12c3ff9b32da05d55ac7feb4cab24c5f7aa9
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2024-03-22 00:38:48 -0300

    improve: exporting file from folders: data and hooks

---
+++ src/app/(admin)/cart/utils/index.ts
@@ -0,0 +1,5 @@
+export { HeaderData } from './data/headerData';
+export { RowDatas } from './data/rowData';
+
+export { useCart } from './hooks/useCart';
+export { useModal } from './hooks/useModal';
