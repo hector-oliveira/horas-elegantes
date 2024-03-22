cec66a520c249f918a74b04b89037e55df0249d3
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2024-03-22 11:25:16 -0300

    feat: new Modal component - EditModa and ViewModal

---
+++ src/components/Modal/EditModal.tsx
@@ -0,0 +1,16 @@
+import { RiCloseCircleLine } from 'react-icons/ri';
+import { ButtonAction } from '../ButtonAction/ButtonAction';
+import { ModalProps } from './interface/Modal.interface';
+
+export const EditModal = ({ closeModal, children }: ModalProps) => {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-60 flex justify-center items-center backdrop-blur-sm">
+      <main className="relative bg-[#F8F8F8] rounded-lg h-4/5 w-3/4 p-10">
+        <ButtonAction className="absolute top-3 right-3" onClick={closeModal}>
+          <RiCloseCircleLine size={30} color="#b85b0e" />
+        </ButtonAction>
+        {children}
+      </main>
+    </div>
+  );
+};
