0f89c77e832f0c472f93bd0b9873f260424fb384
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2024-03-22 12:12:17 -0300

    feat: added OrderDetails compoent in cart page

--- src/app/(admin)/cart/page.tsx
+++ src/app/(admin)/cart/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 import * as C from '@/components/index';
 import * as U from './utils/index';
+import { OrderDetails } from './utils/components/OrderDetails';
 
 export default function Cart() {
   const { filter, setFilter } = U.useCart();
   const {
     isViewModalVisible,
     isEditModalVisible,
-    currentOrder,
+    viewOrder,
+    editOrder,
     handleOpenViewModal,
     handleCloseViewModal,
     handleOpenEditModal,
@@ -32,19 +34,13 @@ export default function Cart() {
           {isViewModalVisible && (
             <C.ViewModal closeModal={handleCloseViewModal}>
               <h1>Modal Visualização</h1>
-              <h1>Pedido: {currentOrder.orderCode}</h1>
-              <h1>Cliente: {currentOrder.client}</h1>
-              <h1>Endereço: {currentOrder.address}</h1>
-              <h1>Status: {currentOrder.status}</h1>
+              <OrderDetails order={viewOrder} />
             </C.ViewModal>
           )}
           {isEditModalVisible && (
             <C.EditModal closeModal={handleCloseEditModal}>
               <h1>Modal Edição</h1>
-              <h1>Pedido: {currentOrder.orderCode}</h1>
-              <h1>Cliente: {currentOrder.client}</h1>
-              <h1>Endereço: {currentOrder.address}</h1>
-              <h1>Status: {currentOrder.status}</h1>
+              <OrderDetails order={editOrder} />
             </C.EditModal>
           )}
         </main>
