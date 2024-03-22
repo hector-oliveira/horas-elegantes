fd46000ec4b7e23c53247029276e45e5ae072c4d
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2024-03-22 11:17:08 -0300

    improve: page cart with table

--- src/app/(admin)/cart/page.tsx
+++ src/app/(admin)/cart/page.tsx
@@ -4,6 +4,15 @@ import * as U from './utils/index';
 
 export default function Cart() {
   const { filter, setFilter } = U.useCart();
+  const {
+    isViewModalVisible,
+    isEditModalVisible,
+    currentOrder,
+    handleOpenViewModal,
+    handleCloseViewModal,
+    handleOpenEditModal,
+    handleCloseEditModal
+  } = U.useModal();
 
   return (
     <C.BaseLayout>
@@ -14,7 +23,30 @@ export default function Cart() {
           onChange={(e) => setFilter(e.target.value)}
         />
         <main className="overflow-y-scroll max-h-96 px-2 shadow-lg">
-          <C.Table hederData={U.HeaderData} bodyData={U.RowDatas} />
+          <C.Table
+            hederData={U.HeaderData}
+            bodyData={U.RowDatas}
+            openViewModal={handleOpenViewModal}
+            openEditModal={handleOpenEditModal}
+          />
+          {isViewModalVisible && (
+            <C.ViewModal closeModal={handleCloseViewModal}>
+              <h1>Modal Visualização</h1>
+              <h1>Pedido: {currentOrder.orderCode}</h1>
+              <h1>Cliente: {currentOrder.client}</h1>
+              <h1>Endereço: {currentOrder.address}</h1>
+              <h1>Status: {currentOrder.status}</h1>
+            </C.ViewModal>
+          )}
+          {isEditModalVisible && (
+            <C.EditModal closeModal={handleCloseEditModal}>
+              <h1>Modal Edição</h1>
+              <h1>Pedido: {currentOrder.orderCode}</h1>
+              <h1>Cliente: {currentOrder.client}</h1>
+              <h1>Endereço: {currentOrder.address}</h1>
+              <h1>Status: {currentOrder.status}</h1>
+            </C.EditModal>
+          )}
         </main>
       </div>
     </C.BaseLayout>
