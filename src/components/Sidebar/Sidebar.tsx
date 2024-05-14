e95bb02c47312af949eba2f08783c784f2a75e6d
Hector Oliveira <hectorabreu_oliveira@hotmail.com>
2024-05-14 16:16:12 -0300

    improve: Sidebar Component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,22 +1,17 @@
 'use client';
 // import React from 'react';
-import Link from 'next/link';
 import { sidebarItems } from './data/sidebarItems.data';
+import { ActiveLink } from '../ActiveLink/ActiveLink';
 export const Sidebar = () => {
   return (
     <div>
       <aside className="w-36 h-screen p-4 bg-@maastricht-blue shadow-xl">
         <ul className="flex flex-col w-full mt-40 gap-4">
           {sidebarItems.map((item) => (
-            <li key={item.label}>
-              <Link href={item.href}>
-                <div className="flex justify-center">
-                  <item.icon
-                    size={42}
-                    className="bg-@snow-white p-2 rounded-full cursor-pointer hover:bg-@botticelli-blue"
-                  />
-                </div>
-              </Link>
+            <li key={item.label} className="flex justify-center">
+              <ActiveLink href={item.href}>
+                <item.icon size={35} />
+              </ActiveLink>
             </li>
           ))}
         </ul>
